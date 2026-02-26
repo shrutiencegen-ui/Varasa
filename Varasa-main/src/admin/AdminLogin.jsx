317de133ce1a02b79d43a76abbb401c75eeb3235
ShrutiJadhav14 <159283106+ShrutiJadhav14@users.noreply.github.com>
2026-02-26 11:15:46 +0530

    finished:

--- Varasa-main/src/admin/AdminLogin.jsx
+++ Varasa-main/src/admin/AdminLogin.jsx
@@ -37,7 +37,7 @@ export default function AdminLogin() {
 
     try {
       setLoading(true);
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("https://varasa-1.onrender.com/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
